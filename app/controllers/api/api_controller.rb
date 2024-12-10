c9397e013d22321c9380c9b85242d009470bde5b
JD Warren <jd.warren@unosquare.com>
2024-12-10 10:10:58 -0600

    fix unauthorized response

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,22 +1,22 @@
 module Api
-  #class ApiController < ::ActionController::API
   class ApiController < ApplicationController
     before_action :authenticate_user
 
     private
 
     def authenticate_user
-      # render status: :unauthorized, json: { error: "token." }
-
-      authenticate_or_request_with_http_token do |token, options|
-        #ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
-        return false if token.blank?
+      authenticate_with_http_token do |token, options|
+        return unauthorized if token.blank?
 
         api_key = ApiKey.find_by(token: token)
-        return false unless api_key.present?
+        return unauthorized unless api_key.present?
 
         @current_user = api_key.user
       end
+    end
+
+    def unauthorized
+      render status: :unauthorized, json: { error: "Unauthorized." }
     end
   end
 end
